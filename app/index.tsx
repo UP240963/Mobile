c1f7e3b8e73c66564ca7bbbc8dff0a69c0ff3134
pythonUP240963 <up240963@alumnos.upa.edu.mx>
2026-03-02 15:43:25 -0600

    add getPokemons fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  
+const getPokemons = async() => {
+  useEffect(() => {
+    console.log("Entre en pantalla");
+    getPokemons();
+  }, []);
+
+  try {
+    const url = "https://poke.co/api/v2/pokemon?limit=100000&offset=0"; 
+  const response = await fetch(url, {
+    method: "GET",
+  });
+  if(response.ok){
+    console.log("Respuesta ok");
+  }else{
+    console.log("Bad request");
+  }
+  } catch (error) {
+    console.log("Error en la petición:", error);
+  }
+};
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>    
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
-  );
-}
+  );}
